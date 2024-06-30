5ac31786549557006a386ca857cdf9ab810493b9
jules <jules.brss@gmail.com>
2024-06-30 17:30:23 +0200

    feat: add module status

---
+++ module_status.h
@@ -0,0 +1,14 @@
+/*
+ * i2c_slave.h
+ *
+ *  Created on: Jun 30, 2024
+ *      Author: jules
+ */
+
+#ifndef INC_MODULE_STATUS_H_
+#define INC_MODULE_STATUS_H_
+
+#define MODULE_STATUS_OK		(0 << 0)
+#define MODULE_STATUS_BOOTING	(1 << 0)
+
+#endif /* INC_MODULE_STATUS_H_ */
